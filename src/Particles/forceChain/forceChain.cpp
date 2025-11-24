2dfe66edb99aee1575e0b60cf9b474266b12aee6
Nima Joghataei <nimajoghataii@gmail.com>
2025-11-25 00:35:45 +0330

    Update forceChain.cpp

--- src/Particles/forceChain/forceChain.cpp
+++ src/Particles/forceChain/forceChain.cpp
@@ -1,65 +1,62 @@
 #include "forceChain.hpp"
-#include "systemControl.hpp"
-#include "dictionary.hpp"
-#include "types.hpp"
 
-namespace pFlow
-{
 
-forceChain::forceChain(
-    systemControl& control, 
+pFlow::forceChain::forceChain(
+    systemControl& control,
     dynamicPointStructure& dynPointStruct
 )
- : forceChainActive_(false),
-    forceChainFCn_(nullptr),
-    forceChainDist_(nullptr), 
-    forceChainPairs_(nullptr),
-    pairCounter_(nullptr)
+:
 
+
+
+  forceChainActive_(false)
+   
 {
 
 }
-
-forceChain::~forceChain()
+pFlow::forceChain::~forceChain()
 {
-    // Clean up allocated fields
+    forceChainFCn_.reset();
+    forceChainDist_.reset();
+    forceChainPairs_.reset();
+    pairCounter_.reset();
     
 }
 
-void forceChain::zeroFCn()
+void pFlow::forceChain::zeroFCn()
 {
     if (!forceChainActive_ || !forceChainFCn_) return;
-    forceChainFCn_->fill(zero3);  // Changed . to ->
+    forceChainFCn_->fill(zero3);  
 }
 
-void forceChain::zeroDist()
+void pFlow::forceChain::zeroDist()
 {
     if (!forceChainActive_ || !forceChainDist_) return;
-    forceChainDist_->fill(zero3);  // Changed . to ->
+    forceChainDist_->fill(zero3);  
 }
 
-void forceChain::zeroPairs()
+void pFlow::forceChain::zeroPairs()
 {
     if (!forceChainActive_ || !forceChainPairs_) return;
-    forceChainPairs_->fill(zero3);  // Changed . to ->
+    forceChainPairs_->fill(zero3);  
 }
 
-void forceChain::zeroAll()
+void pFlow::forceChain::zeroAll()
 {
     if (!forceChainActive_) return;
     zeroFCn();
     zeroDist();
     zeroPairs();
-    if (pairCounter_) pairCounter_->fill(0);  // Changed . to ->
+    if (pairCounter_) pairCounter_->fill(0);  
 }
 
-void forceChain::resetPairCounter()
+void pFlow::forceChain::resetPairCounter()
 {
     if (!forceChainActive_ || !pairCounter_) return;
-    Kokkos::deep_copy(pairCounter_->deviceViewAll(), 0u);  // Changed . to ->
+    Kokkos::deep_copy(pairCounter_->deviceViewAll(), 0u);  
 }
 
-bool forceChain::initializeFromDict(const dictionary& modelDict)
+bool pFlow::forceChain::initializeFromDict(const dictionary& modelDict)
 {
     word forceChainVal = "No";
     forceChainVal = modelDict.getValOrSet<word>("forceChain", forceChainVal);
@@ -82,7 +79,7 @@ bool forceChain::initializeFromDict(const dictionary& modelDict)
 }
 
 
-void forceChain::activateForceChain(systemControl& control, dynamicPointStructure& dynPointStruct)
+void pFlow::forceChain::activateForceChain(systemControl& control, dynamicPointStructure& dynPointStruct)
 {
      if (!forceChainActive_)
         return;
@@ -92,7 +89,7 @@ void forceChain::activateForceChain(systemControl& control, dynamicPointStructur
         forceChainFCn_ = makeUnique<realx3PointField_D>(
             objectFile(
                 "forceChainFCn",
-                "",
+                "forceChain",
                 objectFile::READ_IF_PRESENT,
                 objectFile::WRITE_ALWAYS
             ),
@@ -102,8 +99,8 @@ void forceChain::activateForceChain(systemControl& control, dynamicPointStructur
         
         forceChainDist_ = makeUnique<realx3PointField_D>(
             objectFile(
-                "distance",
-                "",
+                "pos",
+                "forceChain",
                 objectFile::READ_IF_PRESENT,
                 objectFile::WRITE_ALWAYS
             ),
@@ -114,7 +111,7 @@ void forceChain::activateForceChain(systemControl& control, dynamicPointStructur
         forceChainPairs_ = makeUnique<realx3PointField_D>(
             objectFile(
                 "pairs",
-                "",
+                "forceChain",
                 objectFile::READ_IF_PRESENT,
                 objectFile::WRITE_ALWAYS
             ),
@@ -125,7 +122,7 @@ void forceChain::activateForceChain(systemControl& control, dynamicPointStructur
         pairCounter_ = makeUnique<uint32PointField_D>(
             objectFile(
                 "pairCount",
-                "",
+                "forceChain",
                 objectFile::READ_NEVER,
                 objectFile::WRITE_NEVER
             ),
@@ -135,7 +132,48 @@ void forceChain::activateForceChain(systemControl& control, dynamicPointStructur
     }
 }
 
-bool forceChain::isActive() const 
+void pFlow::forceChain::addInteraction(uint32 i, uint32 j, const realx3& FCn, const realx3& xi, const realx3& xj)
+{
+    if (!forceChainActive_ || !forceChainFCn_ || !forceChainDist_ || !forceChainPairs_ || !pairCounter_) 
+        return;
+    
+    // Get device views
+    auto& fcnView = forceChainFCn_->deviceViewAll();
+    auto& distView = forceChainDist_->deviceViewAll();
+    auto& pairsView = forceChainPairs_->deviceViewAll();
+    auto& counterView = pairCounter_->deviceViewAll();
+    
+    // 1. Add forces (atomic operations)
+    Kokkos::atomic_add(&fcnView(i).x_, FCn.x_);
+    Kokkos::atomic_add(&fcnView(i).y_, FCn.y_);
+    Kokkos::atomic_add(&fcnView(i).z_, FCn.z_);
+
+    Kokkos::atomic_add(&fcnView(j).x_, -FCn.x_);
+    Kokkos::atomic_add(&fcnView(j).y_, -FCn.y_);
+    Kokkos::atomic_add(&fcnView(j).z_, -FCn.z_);
+    
+    // 2. Add distances (atomic operations)
+    Kokkos::atomic_add(&distView(i), xi);
+    Kokkos::atomic_add(&distView(j), xj);
+    
+    // 3. Add pair (only once per unique pair, i < j)
+    if (i < j)
+    {
+        real fcn_mag = length(FCn);
+        
+        // Get a unique index using atomic counter
+        uint32_t pairIndex = Kokkos::atomic_fetch_add(&counterView(0), uint32_t(1));
+        
+        // Bounds check before writing
+        const uint32_t maxPairs = static_cast<uint32_t>(pairsView.extent(0));
+        if (pairIndex < maxPairs)
+        {
+            // Single write of the tuple (i,j,fcn_mag)
+            pairsView(pairIndex) = realx3(real(i), real(j), fcn_mag);
+        }
+    }
+}
+bool pFlow::forceChain::isActive() const 
 {
     return forceChainActive_;
 }
@@ -144,4 +182,4 @@ bool forceChain::isActive() const
 
 
 
-} // namespace pFlow
+ 
